60496ed85322443f7597d345906e54a9a494ea0e
Igor Burago <igor@igorburago.com>
2023-12-18 21:56:27 +0800

    libmemdraw: fix cosmetic inconsistencies in writememimage() code

--- src/libmemdraw/write.c
+++ src/libmemdraw/write.c
@@ -10,7 +10,8 @@
 #define	HMASK	(NHASH-1)
 #define	hupdate(h, c)	((((h)<<HSHIFT)^(c))&HMASK)
 typedef struct Hlist Hlist;
-struct Hlist{
+struct Hlist
+{
 	uchar *s;
 	Hlist *next, *prev;
 };
@@ -48,8 +49,8 @@ writememimage(int fd, Memimage *i)
 	outbuf = malloc(ncblock);
 	hash = malloc(NHASH*sizeof(Hlist));
 	chain = malloc(NMEM*sizeof(Hlist));
-	if(data == 0 || outbuf == 0 || hash == 0 || chain == 0){
-	ErrOut:
+	if(data==nil || outbuf==nil || hash==nil || chain==nil){
+	Errout:
 		free(data);
 		free(outbuf);
 		free(hash);
@@ -59,19 +60,19 @@ writememimage(int fd, Memimage *i)
 	lpu = CHUNK/bpl;
 	if(lpu < 1)
 		lpu = 1;
-	for(miny = r.min.y; miny != r.max.y; miny += dy){
+	for(miny=r.min.y; miny!=r.max.y; miny+=dy){
 		dy = r.max.y-miny;
 		if(dy > lpu)
 			dy = lpu;
 		nb = unloadmemimage(i, Rect(r.min.x, miny, r.max.x, miny+dy),
 			data+(miny-r.min.y)*bpl, dy*bpl);
 		if(nb != dy*bpl)
-			goto ErrOut;
+			goto Errout;
 	}
 	snprint(hdr, sizeof(hdr), "compressed\n%11s %11d %11d %11d %11d ",
 		chantostr(cbuf, i->chan), r.min.x, r.min.y, r.max.x, r.max.y);
 	if(write(fd, hdr, 11+5*12) != 11+5*12)
-		goto ErrOut;
+		goto Errout;
 	edata = data+n;
 	eout = outbuf+ncblock;
 	line = data;
@@ -82,30 +83,30 @@ writememimage(int fd, Memimage *i)
 		cp = chain;
 		h = 0;
 		outp = outbuf;
-		for(n = 0; n != NMATCH; n++)
+		for(n=0; n!=NMATCH; n++)
 			h = hupdate(h, line[n]);
 		loutp = outbuf;
 		while(line != edata){
 			ndump = 0;
 			eline = line+bpl;
-			for(p = line; p != eline; ){
+			for(p=line; p!=eline; ){
 				if(eline-p < NRUN)
 					es = eline;
 				else
 					es = p+NRUN;
-				q = 0;
+				q = nil;
 				runlen = 0;
-				for(hp = hash[h].next; hp; hp = hp->next){
+				for(hp=hash[h].next; hp!=nil; hp=hp->next){
 					s = p + runlen;
 					if(s >= es)
 						continue;
 					t = hp->s + runlen;
-					for(; s >= p; s--)
+					for(; s>=p; s--)
 						if(*s != *t--)
-							goto matchloop;
+							goto Matchloop;
 					t += runlen+2;
 					s += runlen+2;
-					for(; s < es; s++)
+					for(; s<es; s++)
 						if(*s != *t++)
 							break;
 					n = s-p;
@@ -115,7 +116,7 @@ writememimage(int fd, Memimage *i)
 						if(n == NRUN)
 							break;
 					}
-			matchloop: ;
+				Matchloop:;
 				}
 				if(runlen < NMATCH){
 					if(ndump == NDUMP){
@@ -128,8 +129,7 @@ writememimage(int fd, Memimage *i)
 					}
 					dumpbuf[ndump++] = *p;
 					runlen = 1;
-				}
-				else{
+				}else{
 					if(ndump != 0){
 						if(eout-outp < ndump+1)
 							goto Bfull;
@@ -144,12 +144,12 @@ writememimage(int fd, Memimage *i)
 					*outp++ = ((runlen-NMATCH)<<2) + (offs>>8);
 					*outp++ = offs&255;
 				}
-				for(q = p+runlen; p != q; p++){
-					if(cp->prev)
-						cp->prev->next = 0;
+				for(q=p+runlen; p!=q; p++){
+					if(cp->prev != nil)
+						cp->prev->next = nil;
 					cp->next = hash[h].next;
 					cp->prev = &hash[h];
-					if(cp->next)
+					if(cp->next != nil)
 						cp->next->prev = cp;
 					cp->prev->next = cp;
 					cp->s = p;
@@ -172,7 +172,7 @@ writememimage(int fd, Memimage *i)
 		}
 	Bfull:
 		if(loutp == outbuf)
-			goto ErrOut;
+			goto Errout;
 		n = loutp-outbuf;
 		snprint(hdr, sizeof(hdr), "%11d %11ld ", r.max.y, n);
 		write(fd, hdr, 2*12);
