17f310620a7ddfc6cbdf4ccb7b3572b8269d5519
Igor Burago <igor@igorburago.com>
2024-01-05 21:13:53 +0800

    9term: fix broken alphabetic sorting of declarations in header files

--- src/cmd/9term/fns.h
+++ src/cmd/9term/fns.h
@@ -1,32 +1,32 @@
 #undef isalnum
 #define isalnum runeisalnum
 
-void	freescrtemps(void);
-Window *new(Image*, int, int, int, char*, char*, char**);
-void	riosetcursor(Cursor*, int);
-int	min(int, int);
-int	max(int, int);
-Rune*	strrune(Rune*, Rune);
-int	isalnum(Rune);
-void	timerstop(Timer*);
-void	timercancel(Timer*);
-Timer*	timerstart(int);
-void	error(char*);
-void	iconinit(void);
-void	*erealloc(void*, uint);
-void *emalloc(uint);
-char *estrdup(char*);
-void	button2menu(Window*);
-void	cvttorunes(char*, int, Rune*, int*, int*, int*);
-char* runetobyte(Rune*, int, int*);
-void	timerinit(void);
-int	rawon(void);
-void	winterrupt(Window*);
-int	intrc(void);
-
 #define	runemalloc(n)		malloc((n)*sizeof(Rune))
 #define	runerealloc(a, n)	realloc(a, (n)*sizeof(Rune))
 #define	runemove(a, b, n)	memmove(a, b, (n)*sizeof(Rune))
 
-void rioputsnarf(void);
-void riogetsnarf(void);
+void*	emalloc(uint);
+void*	erealloc(void*, uint);
+char*	estrdup(char*);
+
+void	button2menu(Window*);
+void	cvttorunes(char*, int, Rune*, int*, int*, int*);
+void	error(char*);
+void	freescrtemps(void);
+void	iconinit(void);
+int	intrc(void);
+int	isalnum(Rune);
+int	max(int, int);
+int	min(int, int);
+Window*	new(Image*, int, int, int, char*, char*, char**);
+int	rawon(void);
+void	riogetsnarf(void);
+void	rioputsnarf(void);
+void	riosetcursor(Cursor*, int);
+char*	runetobyte(Rune*, int, int*);
+Rune*	strrune(Rune*, Rune);
+void	timercancel(Timer*);
+void	timerinit(void);
+Timer*	timerstart(int);
+void	timerstop(Timer*);
+void	winterrupt(Window*);
